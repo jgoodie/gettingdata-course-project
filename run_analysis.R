465492f92c07ab2baf0cce78a41b821fbec8338b
jgoodie <john.goodman@gmail.com>
2015-11-13 10:55:23 -0800

    code is possibly done?

--- run_analysis.R
+++ run_analysis.R
@@ -1,23 +1,26 @@
 #################################################################################################
 # Notes: 
 #
-# SubjectID: Test subject ID and column name
+# subject_id: Test subject ID and column name
 # 
-# Activity: 1=WALKING, 2=WALKING_UPSTAIRS, 3=WALKING_DOWNSTAIRS, 4=SITTING, 5=STANDING, 6=LAYING
-# 
-# Case: Created a new column for training vs test under the Case variable. Values are either 
-# training or testing.
+# activity: 1=WALKING, 2=WALKING_UPSTAIRS, 3=WALKING_DOWNSTAIRS, 4=SITTING, 5=STANDING, 6=LAYING
 # 
 # The features variable grabs all the features from the features.txt file and assignes them as 
-# column names. Note further down the feature names will need
+# column names. Note there are dupliates in this but this is not as issue as we will create
+# unique varible names and clean up the extraneous characters then set to lower case.
 # 
 # The merged variable is the whole data set mashed together.
+#
+# The pared_data variable contains subject_id, activity, mean and standard deviation 
+# data.
 #
 #################################################################################################
 
 # Load any additional libraries
 library(data.table)
+library(plyr)
 library(dplyr)
+library(reshape2)
 
 # Check for and create GettingData folder
 if(!file.exists("GettingData")){
@@ -51,8 +54,6 @@ features <- read.table("GettingData/UCI HAR Dataset/features.txt")
 features<-as.vector(features[,2])
 subject_id <- rbind(subject_train,subject_test)
 colnames(subject_id) <- "subject_ID"
-#Case <- c(rep(1,7352), rep(2,2947))
-case <- c(rep("training",7352), rep("testing",2947))
 activity <- rbind(y_train,y_test)
 colnames(activity) <- "activity"
 x_set <- rbind(x_train, x_test)
@@ -60,7 +61,7 @@ colnames(x_set) <- features
 
 # Mash everything together like Victor Frankenstein! FrankenData!
 # Curious how big this is? run this: print(object.size(merged), units = "Mb")
-merged <- cbind(subject_id, case, activity, x_set)
+merged <- cbind(subject_id, activity, x_set)
 
 # Note: dplyr doesn't like column names gathered from features.txt and it needs to be fixed.
 # here is the code to fix the column names. 
@@ -75,34 +76,36 @@ for(i in 1:length(new_names)){
 names(merged) <- new_names
 
 # Pare down the data using select() from dplyr
-pared_data<-select(merged,subject_id,case,activity,contains("mean"),contains("std"))
+pared_data<-select(merged,subject_id,activity,contains("mean"),contains("std"))
 
 # Fix the Activity column to reflect the actual activity names
 # I don't like this but I couldn't figure out how to do this with mutate() or transform() or some other more elegant method.
 # Activity: 1=WALKING, 2=WALKING_UPSTAIRS, 3=WALKING_DOWNSTAIRS, 4=SITTING, 5=STANDING, 6=LAYING
 for(j in 1:length(pared_data$activity)){
-        #print(pared_data[j,3])
-        if(pared_data[j,3] == 1){ 
-                pared_data[j,3] <- "WALKING"
+        if(pared_data[j,"activity"] == 1){ 
+                pared_data[j,"activity"] <- "WALKING"
         }
-        else if(pared_data[j,3] == 2) {
-                pared_data[j,3] <- "WALKING_UPSTAIRS" 
+        else if(pared_data[j,"activity"] == 2) {
+                pared_data[j,"activity"] <- "WALKING_UPSTAIRS" 
         }
-        else if(pared_data[j,3] == 3){
-                pared_data[j,3] <- "WALKING_DOWNSTAIRS" 
+        else if(pared_data[j,"activity"] == 3){
+                pared_data[j,"activity"] <- "WALKING_DOWNSTAIRS" 
         }
-        else if(pared_data[j,3] == 4) {
-                pared_data[j,3] <- "SITTING"
+        else if(pared_data[j,"activity"] == 4) {
+                pared_data[j,"activity"] <- "SITTING"
         }
-        else if(pared_data[j,3] == 5) {
-                pared_data[j,3] <- "STANDING"
+        else if(pared_data[j,"activity"] == 5) {
+                pared_data[j,"activity"] <- "STANDING"
         }
-        else if(pared_data[j,3] == 6) {
-                pared_data[j,3] <- "LAYING"
+        else if(pared_data[j,"activity"] == 6) {
+                pared_data[j,"activity"] <- "LAYING"
         }
         else {
-                pared_data[j,3] <- "NA"  
+                # This should never happen..... of course never say never
+                pared_data[j,"activity"] <- "NA"  
         }
 }
 
+merged_tidy <- pared_data %>% group_by(subject_id, activity)
+means_tidy <- merged_tidy %>% summarize_each(funs(mean(., na.rm = TRUE)))
 
